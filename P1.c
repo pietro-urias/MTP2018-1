f30632f0eb384aace0a4c24081a4de1de1280c38
pietro-urias <37587763+pietro-urias@users.noreply.github.com>
2018-06-24 21:13:27 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,35 +1,36 @@
-//Pietro Urias   11721ECP001
-#include <stdio.h>
+//Pietro Urias Martins da Silva
+// 11721ECP001
 
-int main() {
-	
-  int Papaleguas,Willy=0;
-    char ACME[255];
-    scanf("%s",&ACME);
-    for(Papaleguas=0; ACME[Papaleguas]!='\0'; Papaleguas++)
-	{
-        if(Willy == 0)
-        {
-               if(ACME[Papaleguas] == '0'){Willy = 0;}
-            if(ACME[Papaleguas] == '1'){Willy = 1;}
-        }
-           else if(Willy == 1)
-        {
-            if(ACME[Papaleguas] == '1'){Willy = 0;}
-            if(ACME[Papaleguas] == '0'){Willy = 2;}
-        }
-           else if (ACME == 2)
-        {
-            if(ACME[Papaleguas] == '1'){Willy = 2;}
-            if(ACME[Papaleguas] == '0'){Willy = 1;}
-           }
-    }
-    if(Willy == 0)
+#include <stdio.h>
+int main()
+{
+    int x = 0, y = 0;
+    char bina[256];
+    printf("Digite um numero em binario: ");
+    scanf("%s", &bina);     	
+while(bina [y] != '\0')
     {
-        printf ("E multiplo de 3");
+        if(bina [y] == '0')
+        {
+            if(x == 0);
+            else if(x == 1) x = 2;
+            else if(x == 2) x = 1;
+            y++;
+        }
+        else if(bina[y] == '1')
+        {
+            if(x == 0) x = 1;
+            else if(x == 1) x = 0;
+            else if(x == 2);
+            y++;
+        }
+        else
+        {
+            printf("\nO numero digitado nao e' binario");
+            return 0;
+        }
     }
-    else if (Willy != 0) 
-    {
-        printf ("E multiplo de 3");
-    }
+    if(x == 0) printf("\n O numero  %s e' multiplo de 3", bina);
+    else printf("\n O numero  %s nao e' multiplo de 3", bina);
+    return 0;
 }
